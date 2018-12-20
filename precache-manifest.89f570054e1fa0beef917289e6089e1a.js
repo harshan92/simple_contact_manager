a43c5e832f2eba720a973e0995fb8feeddf91565
Harshan Madhuranga <harshanmadhurangaw@gmail.com>
2018-12-20 18:44:44 +0530

    Updates

---
+++ precache-manifest.89f570054e1fa0beef917289e6089e1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b98690cd3dd49800aa3",
+    "url": "/simple_contact_manager/static/js/runtime~main.4b98690c.js"
+  },
+  {
+    "revision": "682285ad4890a4ea9b6c",
+    "url": "/simple_contact_manager/static/js/main.682285ad.chunk.js"
+  },
+  {
+    "revision": "34e046b2244f7efbf52b",
+    "url": "/simple_contact_manager/static/js/1.34e046b2.chunk.js"
+  },
+  {
+    "revision": "34e046b2244f7efbf52b",
+    "url": "/simple_contact_manager/static/css/1.69eebbf9.chunk.css"
+  },
+  {
+    "revision": "9c8fd72ab432f51cdd0253c96fbdd76a",
+    "url": "/simple_contact_manager/index.html"
+  }
+];
